75a3c19079820c8c13d9f0f747f7665b80df919e
tristan <westenskowtristan122@gmail.com>
2021-05-12 20:03:48 -0600

    added variable arrays

--- develop/script.js
+++ develop/script.js
@@ -1,12 +1,69 @@
 // Assignment code here
+let letters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+let specialChar = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "=",
+  "+",
+  "{",
+  "}",
+  "[",
+  "]",
+  ";",
+  ":",
+  "<",
+  ">",
+  "?",
+  "/",
+];
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
