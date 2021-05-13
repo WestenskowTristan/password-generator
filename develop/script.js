6ed156096a556e72edca620cb59b41bbab1525f0
tristan <westenskowtristan122@gmail.com>
2021-05-12 22:24:06 -0600

    added prompts and password generation

--- develop/script.js
+++ develop/script.js
@@ -1,5 +1,5 @@
 // Assignment code here
-let letters = [
+let upperCaseLetters = [
   "A",
   "B",
   "C",
@@ -26,6 +26,36 @@ let letters = [
   "X",
   "Y",
   "Z",
+];
+
+let lowerCaseLetters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "g",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
@@ -57,15 +87,69 @@ let specialChar = [
   "/",
 ];
 
+const generateRandomNum = (num) => {
+  return Math.floor(Math.random() * num);
+};
+
+const generatePassword = (limit) => {
+  let randomPasswordCharacters = [];
+  for (let x = 0; x < limit; x++) {
+    let randomNum = generateRandomNum(4);
+
+    switch (randomNum) {
+      case 0:
+        let randomUpcaseNum = generateRandomNum(25);
+        let upperCaseLetter = upperCaseLetters[randomUpcaseNum];
+        randomPasswordCharacters.push(upperCaseLetter);
+        break;
+      case 1:
+        let randomLoCaseNum = generateRandomNum(25);
+        let loCaseletter = lowerCaseLetters[randomLoCaseNum];
+        randomPasswordCharacters.push(loCaseletter);
+        break;
+      case 2:
+        let randomNumNum = generateRandomNum(9);
+        let randomNumNumNum = numbers[randomNumNum];
+        randomPasswordCharacters.push(randomNumNumNum);
+        break;
+      case 3:
+        let randomSpecialChar = generateRandomNum(23);
+        let randomSpecialCharacter = specialChar[randomSpecialChar];
+        randomPasswordCharacters.push(randomSpecialCharacter);
+        break;
+      default:
+        throw new Error("Unknown number given");
+    }
+  }
+
+  return randomPasswordCharacters.join("");
+};
+
 // Get references to the #generate element
 let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
+  let howLongIsPassword = prompt(
+    "How long would you like your password to be? Please enter in a number between 8 and 128"
+  );
+  if (howLongIsPassword < 8 || howLongIsPassword > 128) {
+    alert("Please enter a number between prompted numbers");
+    writePassword();
+  }
 
-  passwordText.value = password;
+  let wantLowerCase = confirm("Would you like Lower case letters?");
+
+  let wantUpperCase = confirm("Would you like Upper case letters?");
+
+  let wantSpecialChar = confirm("Would you like Special characters?");
+
+  if (howLongIsPassword !== "") {
+    let password = generatePassword(howLongIsPassword);
+    let passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+  }
 }
 
 // Add event listener to generate button
